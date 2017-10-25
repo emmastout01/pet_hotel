6a47e4ee8ed7aad226c2fd6d569ecb4b805518b0
Emma Stout <emma.stout01@gmail.com>
2017-10-25 17:11:11 -0500

    getting delete route working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,11 +4,11 @@ $(document).ready(readyNow);
 var $userID;
 var toDeleteId;
 
-function readyNow(){
+function readyNow() {
     console.log('jq');
-    
-    $('#ownerRegisterButton').on('click', ownerRegClick );
-    $('#petRegisterButton').on('click', petRegClick );
+
+    $('#ownerRegisterButton').on('click', ownerRegClick);
+    $('#petRegisterButton').on('click', petRegClick);
     $('#usernameHere').on('change', onOwnerChange);
     $('#petData').on('click', '#updateBtn', updatePets);
     $('#petData').on('click', '#checkInBtn', checkInClick);
@@ -19,36 +19,8 @@ function readyNow(){
     getPets();
 }
 
-function confirmDelete(){
-    $('#delete-modal').modal();
-    toDeleteId = $(this).closest('tr').data('id');
-    console.log('id to delete', toDeleteId)
-}
-
-function deleteTablerow(){
-    $('#delete-modal').modal('hide');
-    $.ajax({
-        type: 'DELETE',
-        url: '/pets/' + toDeleteId
-    }).done(function(response){
-        $('tr[data-id=' + toDeleteId + ']').fadeOut(400, function(){
-            $(this).remove();
-        })
-    })
-}
-
-function checkInClick(){
-    $('#checkOutBtn').removeClass('hidden');
-    $('#checkInBtn').addClass('hidden');
-}
-
-function checkOutClick(){
-    $('#checkInBtn').removeClass('hidden');
-    $('#checkOutBtn').addClass('hidden');
-}
-
-function ownerRegClick(){
-    console.log('in saveOwner' );
+function ownerRegClick() {
+    console.log('in saveOwner');
     var ownerToSend = {
         firstName: $('#ownerFirst').val(),
         lastName: $('#ownerLast').val()
@@ -57,10 +29,10 @@ function ownerRegClick(){
         type: 'POST',
         url: '/owners',
         data: ownerToSend
-    }).done(function(response){
+    }).done(function (response) {
         console.log('added', response);
         getOwners();
-    }).fail(function(error){
+    }).fail(function (error) {
         console.log('Failed: ', error);
     })
 }
@@ -69,23 +41,29 @@ function getOwners() {
     $.ajax({
         type: 'GET',
         url: '/owners',
-    }).done(function(response){
+    }).done(function (response) {
         console.log('added', response);
         appendOwners(response);
-    }).fail(function(error){
+    }).fail(function (error) {
         console.log('Failed: ', error);
     })
 }
 
-function appendOwners(usernameIn){
+function onOwnerChange(event) {
+    $userID = $(this).find(':selected').data('id');
+    console.log('status change')
+    console.log($userID);
+}
+
+function appendOwners(usernameIn) {
     $('#usernameHere').empty();
     $('#usernameHere').append('<option>Select Owner Name</option>')
     for (var i = 0; i < usernameIn.length; i++) {
-     var username = usernameIn[i];
-     var first = username.first;
-     var last = username.last;
-     $('#usernameHere').append('<option data-id ="' + username.id + '">' + first + ' ' + last + '</option>');
- }
+        var username = usernameIn[i];
+        var first = username.first;
+        var last = username.last;
+        $('#usernameHere').append('<option data-id ="' + username.id + '">' + first + ' ' + last + '</option>');
+    }
 }
 
 function petRegClick() {
@@ -97,18 +75,18 @@ function petRegClick() {
         color: $('#petColor').val(),
         breed: $('#petBreed').val()
     }
-    console.log( 'user id', $userID);
+    console.log('user id', $userID);
     console.log(petToSend);
     $.ajax({
         type: 'POST',
         url: '/pets',
         data: petToSend
-    }).done(function(response){
+    }).done(function (response) {
         console.log('added', response[0].id);
         petId = (response[0].id)
-        console.log(typeof(petId));
+        console.log(typeof (petId));
         postPetID(petId);
-    }).fail(function(error){
+    }).fail(function (error) {
         console.log('Failed: ', error);
     })
 }
@@ -121,22 +99,22 @@ function postPetID(value) {
         type: 'POST',
         url: '/visits',
         data: petIDToSend
-    }).done(function(response){
+    }).done(function (response) {
         console.log('added', response);
         console.log('pet id sent:', petIDToSend);
         getPets();
-    }).fail(function(error){
+    }).fail(function (error) {
         console.log('Failed: ', error);
     })
 }
 
-function getPets(){
+function getPets() {
     $.ajax({
         type: 'GET',
         url: '/pets'
-    }).done(function(response){
+    }).done(function (response) {
         appendTable(response)
-    }).fail(function(error){
+    }).fail(function (error) {
         console.log('Failed:', error)
     })
 }
@@ -146,7 +124,7 @@ function appendTable(petsAndOwners) {
     for (var i = 0; i < petsAndOwners.length; i++) {
         var petData = petsAndOwners[i];
         console.log('pet data', petData);
-        var $tr = $('<tr data-id="'+ i + '"></tr>');
+        var $tr = $('<tr data-id="' + i + '"></tr>');
         var updateBtn = `<button id="updateBtn" class="btn btn-info">Update</button>`;
         var deleteBtn = `<button id="deleteBtn" class="btn btn-danger"><span class="glyphicon glyphicon-trash"</span></button>`;
         var checkInBtn = `<button id="checkInBtn" class="btn btn-success">In</button>`;
@@ -157,7 +135,7 @@ function appendTable(petsAndOwners) {
         $tr.append('<td>' + petData.color + '</td>');
         $tr.append('<td>' + updateBtn + '</td>');
         $tr.append('<td>' + deleteBtn + '</td>');
-        $tr.append('<td>' + checkInBtn + checkOutBtn +'</td>');
+        $tr.append('<td>' + checkInBtn + checkOutBtn + '</td>');
         $('#petData').append($tr);
     }
 
@@ -166,10 +144,45 @@ function appendTable(petsAndOwners) {
 
 function updatePets() {
     console.log('in updatePets');
+
 }
 
-function onOwnerChange(event){
-    $userID = $(this).find(':selected').data('id');
-    console.log('status change')
-    console.log($userID);
+
+function confirmDelete() {
+    $('#delete-modal').modal();
+    toDeleteId = $(this).closest('tr').data('id');
+    console.log('id to delete', toDeleteId)
+}
+
+function deleteTablerow() {
+    $('#delete-modal').modal('hide');
+    $.ajax({
+        type: 'DELETE',
+        url: '/visits/' + toDeleteId
+    }).done(function (response) {
+        reallyDeleteItThisTime();
+    })
+}
+
+function reallyDeleteItThisTime() {
+    $.ajax({
+        type: 'DELETE',
+        url: '/pets/' + toDeleteId
+    }).done(function (response) {
+        $('tr[data-id=' + toDeleteId + ']').fadeOut(400, function () {
+            $(this).remove();
+        })
+    })
+}
+
+
+
+function checkInClick() {
+    $('#checkOutBtn').removeClass('hidden');
+    $('#checkInBtn').addClass('hidden');
+}
+
+function checkOutClick() {
+    $('#checkInBtn').removeClass('hidden');
+    $('#checkOutBtn').addClass('hidden');
 }
